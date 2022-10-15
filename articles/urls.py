af71284e097aba52b80110b86647bc4d37fd8d38
Harpreet Saini <hssaini@ualberta.ca>
2022-10-14 21:41:42 -0600

    first commit

---
+++ articles/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+
+from .views import (
+    article_create_view,
+    article_detail_view,
+    article_search_view
+)
+
+app_name = 'articles'
+urlpatterns = [
+    path(r'', article_search_view, name='search'),
+    path(r'create/', article_create_view, name='create'),
+    path(r'<slug:slug>/', article_detail_view, name='detail'),
+
+]
